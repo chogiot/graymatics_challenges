6a2061c5bca7ef5f4fcc0e5c31d891ba160cc8dc
Giota Chondrou <chogiot@gmail.com>
2022-12-01 12:17:10 +0200

    updated code

--- graymatics_prob_2.py
+++ graymatics_prob_2.py
@@ -1,6 +1,6 @@
+import sys
 class brackets:
    def order_type( self, brack_chars ):
-    
        brack_chars=brack_chars.replace(" ", "")
        while True:
            if '[]' in brack_chars :
@@ -13,13 +13,6 @@ class brackets:
                return not brack_chars
 
 if __name__ == '__main__':
-   brack_chars = '( ) [ ]{ }'
-   print(f'{brackets().order_type(brack_chars)}')
-   brack_chars1 = '( [ )]'
-   print(f'{brackets().order_type(brack_chars1)}')
-   brack_chars2 = '{{[]( }})'
-   print(f'{brackets().order_type(brack_chars2)}')
-   brack_chars3 = '{[() ]}'
-   print(f'{brackets().order_type(brack_chars3)}')
-   brack_chars4 = '{{[]( )}}'
-   print(f'{brackets().order_type(brack_chars4)}')
+    brack_chars= sys.argv[1]
+    print(f'{brackets().order_type(brack_chars)}')
+    
